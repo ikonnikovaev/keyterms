c5c3fc655b6f360edc6681564d43fa58af670ef5
elena <ikonnikovaev@gmail.com>
2022-03-19 18:51:11 +0300

    Stage 3

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,32 +1,45 @@
 # Write your code here
+import string
+
+import nltk
 from lxml import etree
 from nltk.tokenize import word_tokenize
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
 from collections import Counter
-'''
-root = (etree.parse("news.xml")).getroot()
-for n in range(len(root[0])):
-    print(root[0][n][0].text, ":", sep="")
-    most_freq = Counter(word_tokenize(root[0][n][1].text.lower())).most_common()
-    sorted_list = sorted(most_freq, key=lambda item: (item[1], item[0]), reverse=True)
-    for i in range(5):
-        print(sorted_list[i][0], end=" ")
-    print("\n")
-'''
-stories = {}
+
+stopwords_list = stopwords.words('english')
+punct_list = list(string.punctuation)
+#print(punct_list)
+
+def extract_common_words(text, k=5):
+    tokens = word_tokenize(story_text)
+    lemmas = [lemmatizer.lemmatize(token) for token in tokens]
+    filtered_lemmas = [lemma for lemma in lemmas
+                       if lemma not in stopwords_list and lemma not in punct_list]
+    #nouns0 = [word for (word, tag) in nltk.pos_tag(filtered_lemmas)
+             #if tag == 'NN']
+    nouns = [lemma for lemma in filtered_lemmas
+             if nltk.pos_tag([lemma])[0][1] == "NN"]
+
+    freq_counter = Counter(sorted(nouns, reverse=True))
+    most_common_nouns = sorted(freq_counter.most_common(k),
+                                key=lambda x: (x[1], x[0]), reverse=True)
+    return [noun[0] for noun in most_common_nouns]
+
+
+# stories = {}
 xml_path = "news.xml"
+lemmatizer = WordNetLemmatizer()
 tree = etree.parse(xml_path)
 root = tree.getroot()
 corpus = root[0]
 for news in corpus:
-
     headline = news[0].text
     story_text = news[1].text.lower()
-    words = word_tokenize(story_text)
-    freq_counter = Counter(sorted(words, reverse=True))
-    most_common_tokens = sorted(freq_counter.most_common(5),
-                                key=lambda x: (x[1], x[0]), reverse=True)
+    most_common_words = extract_common_words(story_text, k=5)
     print(headline +':')
-    print(' '.join([t[0] for t in most_common_tokens]))
+    print(' '.join(most_common_words))
 
 
 
